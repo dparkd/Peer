63d15c65f6178f7a38922c53ca1c6537f99e619f
Daniel Park <dpark@Daniel-Park-8.local>
2016-08-17 14:46:59 -0400

    spellcheck

--- config.rb
+++ config.rb
@@ -36,6 +36,7 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
 activate :directory_indexes
+
 set :relative_links, true
 # Build-specific configuration
 configure :build do
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
-// This is where it all goes :)
+$(document).ready(function() {
+	$('.play-video').on('click', function() {
+		$('.video').addClass('show-video');
+		$('.video-frame').attr('src', 'https://player.vimeo.com/video/179221793?autoplay=1'); 
+	})
+
+	$('.close-video').on('click', function() {
+		console.log('hello?')
+		$('.video').removeClass('show-video');
+		$('.video-frame').attr('src', ' ');
+	})
+});
