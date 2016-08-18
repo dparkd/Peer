3f1fa65e6964bc858d22af081a4b6bbc356d89ca
Daniel Park <dpark@Daniel-Park-8.local>
2016-08-18 10:01:38 -0400

    fix video button

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 	$('.play-video').on('click', function() {
+		$('.play-video').css('z-index', '0');
 		$('.video').addClass('show-video');
 		$('.video-frame').attr('src', 'https://player.vimeo.com/video/179221793?autoplay=1'); 
 	})
 
 	$('.close-video').on('click', function() {
-		console.log('hello?')
+		$('.play-video').css('z-index', '9999');
 		$('.video').removeClass('show-video');
 		$('.video-frame').attr('src', ' ');
 	})
